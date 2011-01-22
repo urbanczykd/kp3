83e471fea2fc47d058ff6fdfb0293d414c1077f0
urbanczykd <urbanczykd@gmail.com>
2011-01-22 18:11:32 +0100

    sc

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-
+ENV['RAILS_ENV'] ||= 'production'
 Rails::Initializer.run do |config|
 
 config.gem "authlogic"
